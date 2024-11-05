9ee10b3c039e8970e8175a45b7cb497f44735873
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-11-05 06:31:55 +0100

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import mongodb from "./mongodb.js";
 
 const app = express();
 
 
-mongoose.connect("")
+mongoose.connect(mongodb)
 .then(() => app.listen(4000))
 .then(() => console.log("Connection successful please check port 4000!"))
 .catch((err) => console.log(err));
-
-app.listen(5000);
---
+++ backend/mongodb.js
@@ -0,0 +1,3 @@
+const mongodb = "mongodb+srv://Zymo:gly20171025@cluster.sqpsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+
+export default mongodb;
