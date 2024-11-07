b09998a87917b9f67261472e0b9ce3d927c8fc39
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-11-07 12:13:01 +0100

    fixed getById

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -27,5 +27,38 @@ export const addBlog = async (req, res, next) => {
     } catch (err) {
         return res.status(500).json({message: "Error! Blog can not be saved."});
     }
+    return res.status(201).json({blog});
+};
+
+export const getById = async (req, res, next) => {
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findById(blogId);
+    } catch (err) {
+        return res.status(500).json({message: "Error! A blockade was encountered."});
+    }
+    if (!blog) {
+        return res.status(404).json({message: "The blog you are trying to locate doesn't exist."});
+    }
     return res.status(200).json({blog});
 }
+
+export const updateBlog = async (req, res, next) => {
+    const { title, description } = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description
+        }, {new: true});
+    } catch(err) {
+        return res.status(500).json({message: "Error! A blockade was encountered."});
+    }
+    if (!blog) {
+        return res.status(404).json({message: "The blog you are trying to locate doesn't exist."});
+    }
+    return res.status(200).json({blog});
+};
+
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getAllBlogs } from "../controllers/blogController.js";
+import { addBlog, getAllBlogs, updateBlog, getById } from "../controllers/blogController.js";
 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
+blogRouter.get("/:id", getById);
+blogRouter.post("/add", addBlog);
+blogRouter.put("/update/:id", updateBlog);
 
 export default blogRouter;
