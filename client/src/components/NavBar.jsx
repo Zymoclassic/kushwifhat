119e05b3843187a7811a5d6b07daa35065a5027c
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-12-02 11:28:01 +0100

    Nav bar responsiveness

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../assets/images/logo.png";
 import "../assets/css/navbar.css";
 
 const NavBar = () => {
+
+  const [toggle, setToggle] = useState(true);
+
+  const toggleBars = () => setToggle(!toggle)
+
   return (
     <nav>
       <div className="container navContainer">
@@ -11,18 +16,20 @@ const NavBar = () => {
         <img src={Logo} alt="kushwifhat" className="logo" />
         </Link>
 
-        <ul className="navMenu">
+        <div className='navBars' onClick={toggleBars}>
+              <i className='uil uil-bars' ></i>
+        </div>
+
+
+        <ul className= {toggle ? "navMenu" : "navMenuHide"}>
           <li><Link to="/user/:id">Koly Zymo</Link></li>
           <li><Link to="/posts/create">Create</Link></li>
           <li><Link to="/posts/categories/:category">Category</Link></li>
           <li><Link to="/users">Authors</Link></li>
           <li><Link to="/user/logout">Logout</Link></li>
         </ul>
-
-        <button className='navClose'>
-          <i className='uil uil-times'></i>
-        </button>
       </div>
+
     </nav>
   )
 }
